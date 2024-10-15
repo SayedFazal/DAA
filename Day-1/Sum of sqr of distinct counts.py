ee886f5344edfe8deb28b506deda45c3fe196266
SayedFazal <syedfazal2956@gmail.com>
2024-10-15 10:24:24 +0530

    Update Sum of sqr of distinct counts.py

--- Day-1/Sum of sqr of distinct counts.py
+++ Day-1/Sum of sqr of distinct counts.py
@@ -1,17 +1,18 @@
 '''You are given a 0-indexed integer array nums. The distinct count of a subarray of nums is defined as: Let nums[i..j] be a subarray of nums consisting of all the 
 indices from i to j such that 0 <= i <= j < nums.length. Then the number of distinct values in nums[i..j] is called the distinct count of nums[i..j]. Return the sum 
 of the squares of distinct counts of all subarrays of nums. A subarray is a contiguous non-empty sequence of elements within an array.'''
-def sum_of_squares(nums):
-    total_sum = 0
-    for i in range(len(nums)):
-        distinct_elements = []
-        for j in range(i, len(nums)):
-            if nums[j] not in distinct_elements:
-                distinct_elements.append(nums[j])
-            total_sum += len(distinct_elements) ** 2
-    return total_sum
-nums = [1, 2, 1]
-print(sum_of_squares(nums)) 
+def subarray(nums):
+    sum=0
+    n=len(nums)
+    for i in range(0,n):
+        ans=[]
+        for j in range(i,n):
+            if nums[j] not in ans:
+                ans.append(nums[j])
+            sum+=len(ans)**2
+    return sum
+num=[1,2,1]
+print(subarray(num)) 
 
 # Output: 15
 
