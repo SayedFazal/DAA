6358eec4498d977b95c66a01ee3a648158022bb6
SayedFazal <syedfazal2956@gmail.com>
2024-10-15 09:57:32 +0530

    Update First palindrome.py

--- Day-1/First palindrome.py
+++ Day-1/First palindrome.py
@@ -1,12 +1,11 @@
 #Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "". A string is palindromic if it reads the same forward and backward.
-def first_palindromic_string(words):
-    for word in words:
-        if word == word[::-1]:  # Check if the word is a palindrome
-            return word
-    return ""  # Return an empty string if no palindrome is found
-
-# Test with example input
-words = ["abc", "car", "ada", "racecar", "cool"]
-print(first_palindromic_string(words))
+def firstpal(word):
+    for i in word:
+        if(pal(i)):
+            return i
+def pal(s):
+    return s==s[::-1]
+word=["abc", "car", "ada", "racecar", "cool"]
+print(firstpal(word))
 
 Output: "ada"
