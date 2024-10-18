e278f934c2dc00cb67cbf053cfc9b0ea18fddbed
SayedFazal <syedfazal2956@gmail.com>
2024-10-18 09:18:28 +0530

    Update Count pairs.py

--- Day-1/Count pairs.py
+++ Day-1/Count pairs.py
@@ -1,14 +1,12 @@
 #Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k
-def count_pairs(nums, k):
-    count = 0
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] == nums[j] and (i * j) % k == 0:
-                count += 1
-    return count
-
 nums = [3, 1, 2, 2, 2, 1, 3]
 k = 2
-print(count_pairs(nums, k))  
+n=len(nums)
+count = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        if nums[i] == nums[j] and (i * j) % k == 0:
+            count += 1
+print(count)  
 
 # Output: 4
