88e8186a573ba959437c5136b3cf927940e72973
SayedFazal <syedfazal2956@gmail.com>
2024-11-22 20:56:20 +0530

    Update Binary search.py

--- Day-1/Binary search.py
+++ Day-1/Binary search.py
@@ -1,22 +1,13 @@
 '''9. Checks if a given number x exists in a sorted array arr using binary search. Analyze its time complexity using Big-O notation.'''
-def binary_search(arr, key):
-    arr.sort()  # Sorting the array
-    low, high = 0, len(arr) - 1
+arr = [2, 3, 4, 10, 40]
+target = 10
 
-    while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] == key:
-            return f"Element {key} is found at position {mid}"
-        elif arr[mid] < key:
-            low = mid + 1
-        else:
-            high = mid - 1
+low, high = 0, len(arr) - 1
+while low <= high:
+    mid = (low + high) // 2
+    if arr[mid] == target: print(f"Found at {mid}"); break
+    elif arr[mid] < target: low = mid + 1
+    else: high = mid - 1
+else: print("Not found")
 
-    return f"Element {key} not found"
-
-# Test case
-arr = [3, 4, 6, -9, 10, 8, 9, 30]
-key = 10
-print(binary_search(arr, key))
-
-Output:Element 10 is found at position 6
+Output:Element 10 is found at position 4
